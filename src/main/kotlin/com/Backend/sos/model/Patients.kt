4eb4f3eb04c7fb1f828ec6265b832fa677dddad9
7Jeremy7 <116099030+7Jeremy7@users.noreply.github.com>
2024-05-17 16:58:59 -0500

    model-Patients

---
+++ src/main/kotlin/com/Backend/sos/model/Patients.kt
@@ -0,0 +1,26 @@
+package com.Backend.sos.model
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "patients")
+class Patients {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(updatable = false)
+    var id: Long? = null
+    @Column(name = "user_id")
+    var userId: Long? = null
+    var ptFirstname: String? = null
+    var ptLastname: String? = null
+    var ptOccupation: String? = null
+    var ptAddress: String? = null
+    var ptPhone: String? = null
+    var ptAge: Long? = null
+    var ptCi: String? = null
+    var ptCity: String? = null
+    var ptEmail: String? = null
+    var ptConsultationReason: String? = null
+    var ptRecommendations: String? = null
+
+}
