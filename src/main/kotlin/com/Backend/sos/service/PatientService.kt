e81526fbc8acca3358cd5fe83e275e4b418f0d38
7Jeremy7 <116099030+7Jeremy7@users.noreply.github.com>
2024-05-23 07:06:36 -0500

    ñ

--- src/main/kotlin/com/Backend/sos/service/PatientService.kt
+++ src/main/kotlin/com/Backend/sos/service/PatientService.kt
@@ -2,6 +2,7 @@ package com.Backend.sos.service
 
 import com.Backend.sos.model.Patients
 import com.Backend.sos.repository.PatientRepository
+import com.Backend.sos.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
@@ -12,13 +13,20 @@ class PatientService {
     @Autowired
     lateinit var patientRepository: PatientRepository
 
+    @Autowired
+    lateinit var userRepository: UserRepository
+
     fun list():List<Patients>?{
         return patientRepository.findAll()
     }
 
     fun save (model: Patients): Patients{
+
         try {
+            userRepository.findById(model.userId)
+                ?: throw Exception("Id del cliente no encontrada")
             return patientRepository.save(model)
+
         }
         catch (ex:Exception){
             throw ResponseStatusException(HttpStatus.NOT_FOUND,ex.message)
